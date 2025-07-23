958bd7bfd6f922bd138b7cd6b510a6c4c06d6262
MidoriKami <mi_kami@outlook.com>
2025-07-22 22:45:07 -0700

    Add abstract config base classes

---
+++ VanillaPlus/Core/Objects/GameModificationCharacterConfig.cs
@@ -0,0 +1,13 @@
+﻿using VanillaPlus.Utilities;
+
+namespace VanillaPlus.Core.Objects;
+
+/// <summary>
+/// For use with per-character settings. This will save a file in a character specific directory.
+/// </summary>
+/// <remarks>You must be logged in to load or save a character config.</remarks>
+public abstract class GameModificationCharacterConfig<T> where T : GameModificationCharacterConfig<T>, new() {
+    protected abstract string FileName { get; }
+    public static T Load() => Configuration.LoadCharacterConfig<T>(new T().FileName);
+    public void Save() => Configuration.SaveCharacterConfig(this, FileName);
+}
---
+++ VanillaPlus/Core/Objects/GameModificationConfig.cs
@@ -0,0 +1,9 @@
+﻿using VanillaPlus.Utilities;
+
+namespace VanillaPlus.Core.Objects;
+
+public abstract class GameModificationConfig<T> where T : GameModificationConfig<T>, new() {
+    protected abstract string FileName { get; }
+    public static T Load() => Configuration.LoadConfig<T>(new T().FileName);
+    public void Save() => Configuration.SaveConfig(this, FileName);
+}
