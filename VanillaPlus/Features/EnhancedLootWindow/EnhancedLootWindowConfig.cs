3a0c4df18b370dbdf764cb418fc9c4034ae9cecb
MidoriKami <mi_kami@outlook.com>
2025-07-22 22:43:19 -0700

    Use abstract type for Config file

--- VanillaPlus/Features/EnhancedLootWindow/EnhancedLootWindowConfig.cs
+++ VanillaPlus/Features/EnhancedLootWindow/EnhancedLootWindowConfig.cs
@@ -1,14 +1,10 @@
-﻿using VanillaPlus.Utilities;
+﻿using VanillaPlus.Core.Objects;
 
 namespace VanillaPlus.Features.EnhancedLootWindow;
 
-public class EnhancedLootWindowConfig {
+public class EnhancedLootWindowConfig : GameModificationConfig<EnhancedLootWindowConfig> {
+    protected override string FileName => "EnhancedLootWindow.config.json";
+   
     public bool MarkUnobtainableItems = true;
     public bool MarkAlreadyObtainedItems = true;
-
-    public static EnhancedLootWindowConfig Load()
-        => Configuration.LoadConfig<EnhancedLootWindowConfig>("EnhancedLootWindow.config.json");
-    
-    public void Save()
-        => Configuration.SaveConfig(this, "EnhancedLootWindow.config.json");
 }
