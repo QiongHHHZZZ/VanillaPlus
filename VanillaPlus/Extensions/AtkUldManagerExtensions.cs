f258b4b453c71f5944b7a95a5a942c53c4068e66
MidoriKami <mi_kami@outlook.com>
2025-07-22 19:39:49 -0700

    Add own version of SearchNodeById, because the games version is dumb.

---
+++ VanillaPlus/Extensions/AtkUldManagerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace VanillaPlus.Extensions;
+
+public static class AtkUldManagerExtensions {
+    public static unsafe T* SearchNodeById<T>(this AtkUldManager atkUldManager, uint nodeId) where T : unmanaged {
+        foreach (var node in atkUldManager.Nodes) {
+            if (node.Value is not null) {
+                if (node.Value->NodeId == nodeId)
+                    return (T*) node.Value;
+            }
+        }
+
+        return null;
+    }
+
+    public static unsafe AtkResNode* SearchNodeById(this AtkUldManager atkUldManager, uint nodeId)
+        => atkUldManager.SearchNodeById<AtkResNode>(nodeId);
+}
