09eecf399c92fbb193daee37617450312afcdb0b
MidoriKami <mi_kami@outlook.com>
2025-07-23 19:11:12 -0700

    Add ActionBarSlot Extensions

---
+++ VanillaPlus/Extensions/ActionBarSlotExtensions.cs
@@ -0,0 +1,25 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace VanillaPlus.Extensions;
+
+public static unsafe class ActionBarSlotExtensions {
+    public static AtkImageNode* GetImageNode(this ref ActionBarSlot slot) {
+        var component = slot.GetIconComponent();
+        if (component is null) return null;
+
+        return component->IconImage;
+    }
+
+    public static AtkResNode* GetFrameNode(this ref ActionBarSlot slot) {
+        var component = slot.GetIconComponent();
+        if (component is null) return null;
+
+        return component->Frame;
+    }
+
+    private static AtkComponentIcon* GetIconComponent(this ref ActionBarSlot slot) {
+        if (slot.Icon is null) return null;
+        return (AtkComponentIcon*) slot.Icon->Component;
+    }
+}
