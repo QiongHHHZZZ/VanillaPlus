0ae1c693b79e8e8141c79b3a95e9d34dd3ae3090
MidoriKami <mi_kami@outlook.com>
2025-07-23 19:10:48 -0700

    Add Dalamud.Window Extensions

---
+++ VanillaPlus/Extensions/WindowExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Dalamud.Interface.Windowing;
+
+namespace VanillaPlus.Extensions;
+
+public static class WindowExtensions {
+    public static void OpenWindow(this Window window)
+        => window.IsOpen = true;
+
+    public static void ToggleWindow(this Window window)
+        => window.IsOpen = !window.IsOpen;
+
+    public static void CloseWindow(this Window window)
+        => window.IsOpen = false;
+
+    public static void RemoveFromWindowSystem(this Window window)
+        => System.WindowSystem.RemoveWindow(window);
+    
+    public static void AddToWindowSystem(this Window window)
+        => System.WindowSystem.AddWindow(window);
+}
