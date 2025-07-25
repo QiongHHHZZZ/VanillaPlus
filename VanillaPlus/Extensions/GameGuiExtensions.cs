435fa04539f0bbd9996f1e920df5acab9236cad1
MidoriKami <mi_kami@outlook.com>
2025-07-25 08:47:14 -0700

    Add GameGuiExtensions.cs

---
+++ VanillaPlus/Extensions/GameGuiExtensions.cs
@@ -0,0 +1,8 @@
+using Dalamud.Plugin.Services;
+
+namespace VanillaPlus.Extensions;
+
+public static class GameGuiExtensions {
+    public static unsafe T* GetAddonByName<T>(this IGameGui gameGui, string name) where T : unmanaged
+        => (T*) gameGui.GetAddonByName(name);
+}
