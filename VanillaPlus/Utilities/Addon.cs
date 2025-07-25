ad00b90dc78e3596e77134904fdc0503258d1d8c
MidoriKami <mi_kami@outlook.com>
2025-07-24 18:32:27 -0700

    Add some addon utilities

--- VanillaPlus/Utilities/Addon.cs
+++ VanillaPlus/Utilities/Addon.cs
@@ -1,5 +1,17 @@
-﻿namespace VanillaPlus.Utilities;
+﻿using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.System;
 
-public static class Addon {
-    
+namespace VanillaPlus.Utilities;
+
+public static unsafe class Addon {
+    public static AtkUnitBase* GetAddonForNode(NodeBase node)
+        => RaptureAtkUnitManager.Instance()->GetAddonByNode((AtkResNode*)node);
+
+    public static void UpdateCollisionForNode(NodeBase node) {
+        var addon = GetAddonForNode(node);
+        if (addon is not null) {
+            addon->UpdateCollisionNodeList(false);
+        }
+    }
 }
