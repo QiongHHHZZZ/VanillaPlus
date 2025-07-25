9bfaebba42e08dd378fffef4eea33a650255e22c
MidoriKami <mi_kami@outlook.com>
2025-07-24 19:28:36 -0700

    Add DataManagerExtensions.cs

---
+++ VanillaPlus/Extensions/DataManagerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Dalamud.Plugin.Services;
+using Lumina.Excel.Sheets;
+using Lumina.Text.ReadOnly;
+
+namespace VanillaPlus.Extensions;
+
+public static class DataManagerExtensions {
+    public static ReadOnlySeString GetAddonText(this IDataManager dataManager, uint id)
+        => dataManager.GetExcelSheet<Addon>().GetRow(id).Text;
+}
