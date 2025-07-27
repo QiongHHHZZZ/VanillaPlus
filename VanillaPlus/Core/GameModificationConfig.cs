6ab5626e3b0e13c8dc672ce9ed6d68e482894b59
MidoriKami <mi_kami@outlook.com>
2025-07-27 11:55:16 -0700

    Log file loading and saving

--- VanillaPlus/Core/GameModificationConfig.cs
+++ VanillaPlus/Core/GameModificationConfig.cs
@@ -4,6 +4,15 @@ namespace VanillaPlus.Core;
 
 public abstract class GameModificationConfig<T> where T : GameModificationConfig<T>, new() {
     protected abstract string FileName { get; }
-    public static T Load() => Config.LoadConfig<T>(new T().FileName);
-    public void Save() => Config.SaveConfig(this, FileName);
+    public static T Load() {
+        var configFileName = new T().FileName;
+        
+        Services.PluginLog.Debug($"Loading Config {configFileName}");
+        return Config.LoadConfig<T>(configFileName);
+    } 
+    
+    public void Save() {
+        Services.PluginLog.Debug($"Saving Config {FileName}");
+        Config.SaveConfig(this, FileName);
+    }
 }
