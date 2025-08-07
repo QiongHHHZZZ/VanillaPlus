62fc64c02c9950ab95002a32902a277b86fd472a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2025-08-06 18:23:13 -0700

    Add Better Cursor (#2)
    * Version 0.1.0.5
    
    * Add Better Cursor
    
    * Add image
    
    * Add toggle for hiding on camrea move
    
    * fix wrong frame index for no animation timeline
    
    * Use new KTK features
    
    * API13 Update

---
+++ VanillaPlus/Features/BetterCursor/BetterCursorConfigWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Dalamud.Bindings.ImGui;
+using Dalamud.Interface.Windowing;
+using VanillaPlus.BetterCursor;
+
+namespace VanillaPlus.Features.BetterCursor;
+
+public class BetterCursorConfigWindow(BetterCursorConfig config, Action onConfigChanged) : Window("Better Cursor Config", ImGuiWindowFlags.AlwaysAutoResize) {
+    public override void Draw() {
+        if (ImGui.ColorEdit4("Color", ref config.Color)) {
+            onConfigChanged();
+            config.Save();
+        }
+
+        if (ImGui.DragFloat("Size", ref config.Size)) {
+            onConfigChanged();
+            config.Save();
+        }
+
+        if (ImGui.Checkbox("Enable Animation", ref config.Animations)) {
+            onConfigChanged();
+            config.Save();
+        }
+
+        if (ImGui.Checkbox("Hide on Left-Hold or Right-Hold", ref config.HideOnCameraMove)) {
+            config.Save();
+        }
+    }
+
+    public override void OnClose()
+        => config.Save();
+}
