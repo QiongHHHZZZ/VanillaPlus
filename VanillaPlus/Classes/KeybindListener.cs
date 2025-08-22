136ad4a741cd85a356f5ab7ec207617b922969c8
MidoriKami <mi_kami@outlook.com>
2025-08-22 15:04:43 -0700

    Ignore Keybinds when using input text

--- VanillaPlus/Classes/KeybindListener.cs
+++ VanillaPlus/Classes/KeybindListener.cs
@@ -4,11 +4,12 @@ using System.Diagnostics;
 using System.Linq;
 using Dalamud.Game.ClientState.Keys;
 using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using VanillaPlus.Extensions;
 
 namespace VanillaPlus.Classes;
 
-public class KeybindListener : IDisposable {
+public unsafe class KeybindListener : IDisposable {
     private readonly Stopwatch debouncer = Stopwatch.StartNew();
     
     public required HashSet<VirtualKey> KeyCombo { get; set; }
@@ -23,6 +24,9 @@ public class KeybindListener : IDisposable {
     private void OnFrameworkUpdate(IFramework framework) {
         // Don't process keybinds if we are settings up a new keybind
         if (System.WindowSystem.Windows.Any(window => window.WindowName.Contains("Keybind Modal") && window.IsOpen)) return;
+
+        // Don't process keybinds if any input text is active
+        if (RaptureAtkModule.Instance()->IsTextInputActive()) return;
         
         if (Services.KeyState.IsKeybindPressed(KeyCombo) && debouncer.ElapsedMilliseconds >= 250) {
             Services.KeyState.ResetKeyCombo(KeyCombo);
