985b1414fb20b226b9c5e6100db72614014e26f9
MidoriKami <mi_kami@outlook.com>
2025-08-21 23:31:44 -0700

    Add AddonLifecycleExtensions.cs

---
+++ VanillaPlus/Extensions/AddonLifecycleExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using Dalamud.Plugin.Services;
+
+namespace VanillaPlus.Extensions;
+
+public static class AddonLifecycleExtensions {
+    public static void LogAddon(this IAddonLifecycle addonLifecycle, string addonName) {
+        addonLifecycle.RegisterListener(AddonEvent.PreSetup, addonName, Logger);
+        addonLifecycle.RegisterListener(AddonEvent.PreRefresh, addonName, Logger);
+        addonLifecycle.RegisterListener(AddonEvent.PreReceiveEvent, addonName, Logger);
+        addonLifecycle.RegisterListener(AddonEvent.PreRequestedUpdate, addonName, Logger);
+        addonLifecycle.RegisterListener(AddonEvent.PreFinalize, addonName, Logger);
+    }
+
+    private static void Logger(AddonEvent type, AddonArgs args)
+        => Services.PluginLog.Debug($"{args.AddonName} called {type}");
+
+    public static void UnLogAddon(this IAddonLifecycle addonLifecycle, string addonName) {
+        addonLifecycle.UnregisterListener(AddonEvent.PreSetup, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PreRefresh, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PreReceiveEvent, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PreRequestedUpdate, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PreFinalize, addonName, Logger);
+    }
+}
