49b3f969bf3e831a78d0822d411213ab3041ac72
MidoriKami <mi_kami@outlook.com>
2025-08-23 12:10:48 -0700

    Add Helpers

--- VanillaPlus/Extensions/AddonArgsExtensions.cs
+++ VanillaPlus/Extensions/AddonArgsExtensions.cs
@@ -1,8 +1,29 @@
-﻿using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+﻿using System;
+using System.Linq;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace VanillaPlus.Extensions;
 
 public static unsafe class AddonArgsExtensions {
     public static T* GetAddon<T>(this AddonArgs args) where T : unmanaged
         => (T*)args.Addon.Address;
+
+    public static void PrintAtkValues(this AddonArgs args) {
+        var atkValues = args switch {
+            AddonRefreshArgs refreshArgs => refreshArgs.AtkValueSpan,
+            AddonSetupArgs setupArgs => setupArgs.AtkValueSpan,
+            _ => throw new Exception("Invalid Args Type"),
+        };
+
+        foreach (var index in Enumerable.Range(0, atkValues.Length)) {
+            ref var value = ref atkValues[index];
+            if (value.Type is 0) continue;
+            
+            Services.PluginLog.Debug($"[{index,4}]{value.GetValueAsString()}");
+        }
+    }
+
+    public static Span<AtkValue> GetAtkValues(this AddonArgs args)
+        => args.GetAddon<AtkUnitBase>()->AtkValuesSpan;
 }
--- VanillaPlus/Extensions/AddonLifecycleExtensions.cs
+++ VanillaPlus/Extensions/AddonLifecycleExtensions.cs
@@ -1,26 +1,55 @@
 ﻿using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace VanillaPlus.Extensions;
 
+public record LoggedAddonEvents {
+    public bool Setup { get; set; } = true;
+    public bool Refresh { get; set; } = true;
+    public bool ReceiveEvent { get; set; } = false;
+    public bool RequestedUpdate { get; set; } = true;
+    public bool Update { get; set; } = false;
+    public bool Draw { get; set; } = false;
+    public bool Finalize { get; set; } = true;
+}
+
 public static class AddonLifecycleExtensions {
-    public static void LogAddon(this IAddonLifecycle addonLifecycle, string addonName) {
-        addonLifecycle.RegisterListener(AddonEvent.PreSetup, addonName, Logger);
-        addonLifecycle.RegisterListener(AddonEvent.PreRefresh, addonName, Logger);
-        addonLifecycle.RegisterListener(AddonEvent.PreReceiveEvent, addonName, Logger);
-        addonLifecycle.RegisterListener(AddonEvent.PreRequestedUpdate, addonName, Logger);
-        addonLifecycle.RegisterListener(AddonEvent.PreFinalize, addonName, Logger);
+    public static void LogAddon(this IAddonLifecycle addonLifecycle, string addonName, LoggedAddonEvents? loggedModules = null) {
+        if (loggedModules is not null) {
+            if (loggedModules.Setup) addonLifecycle.RegisterListener(AddonEvent.PostSetup, addonName, Logger);
+            if (loggedModules.Refresh) addonLifecycle.RegisterListener(AddonEvent.PostRefresh, addonName, Logger);
+            if (loggedModules.ReceiveEvent) addonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, addonName, Logger);
+            if (loggedModules.RequestedUpdate) addonLifecycle.RegisterListener(AddonEvent.PostRequestedUpdate, addonName, Logger);
+            if (loggedModules.Update) addonLifecycle.RegisterListener(AddonEvent.PostUpdate, addonName, Logger);
+            if (loggedModules.Draw) addonLifecycle.RegisterListener(AddonEvent.PostDraw, addonName, Logger);
+            if (loggedModules.Finalize) addonLifecycle.RegisterListener(AddonEvent.PreFinalize, addonName, Logger);
+        }
+        else {
+            addonLifecycle.RegisterListener(AddonEvent.PostSetup, addonName, Logger);
+            addonLifecycle.RegisterListener(AddonEvent.PostRefresh, addonName, Logger);
+            addonLifecycle.RegisterListener(AddonEvent.PostReceiveEvent, addonName, Logger);
+            addonLifecycle.RegisterListener(AddonEvent.PostRequestedUpdate, addonName, Logger);
+            addonLifecycle.RegisterListener(AddonEvent.PreFinalize, addonName, Logger);
+        }
     }
 
-    private static void Logger(AddonEvent type, AddonArgs args)
-        => Services.PluginLog.Debug($"{args.AddonName} called {type}");
+    private static void Logger(AddonEvent type, AddonArgs args) {
+        Services.PluginLog.Debug($"{args.AddonName} called {type.ToString().Replace("Post", string.Empty)}");
+
+        if (args is AddonReceiveEventArgs receiveEventArgs) {
+            Services.PluginLog.Debug($"\t{(AtkEventType)receiveEventArgs.AtkEventType}: {receiveEventArgs.EventParam}");
+        }
+    }
 
     public static void UnLogAddon(this IAddonLifecycle addonLifecycle, string addonName) {
-        addonLifecycle.UnregisterListener(AddonEvent.PreSetup, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PreRefresh, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PreReceiveEvent, addonName, Logger);
-        addonLifecycle.UnregisterListener(AddonEvent.PreRequestedUpdate, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PostSetup, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PostRefresh, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PostReceiveEvent, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PostRequestedUpdate, addonName, Logger);
         addonLifecycle.UnregisterListener(AddonEvent.PreFinalize, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PostDraw, addonName, Logger);
+        addonLifecycle.UnregisterListener(AddonEvent.PostUpdate, addonName, Logger);
     }
 }
