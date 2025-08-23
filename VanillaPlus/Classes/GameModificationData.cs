add7b90d63d87f07d665809f156fcf73b5b0c1e6
MidoriKami <mi_kami@outlook.com>
2025-08-22 18:49:31 -0700

    Add GameModificationData

---
+++ VanillaPlus/Classes/GameModificationData.cs
@@ -0,0 +1,18 @@
+﻿using VanillaPlus.Utilities;
+
+namespace VanillaPlus.Classes;
+
+public abstract class GameModificationData<T> where T : GameModificationData<T>, new() {
+    protected abstract string FileName { get; }
+    public static T Load() {
+        var configFileName = new T().FileName;
+        
+        Services.PluginLog.Debug($"Loading Data {configFileName}");
+        return Config.LoadData<T>(configFileName);
+    } 
+    
+    public void Save() {
+        Services.PluginLog.Debug($"Saving Data {FileName}");
+        Config.SaveData(this, FileName);
+    }
+}
