d921f8add8ed9b64f632166254ef08de8be73b07
MidoriKami <mi_kami@outlook.com>
2025-08-27 17:07:54 -0700

    Remove Drawing.KeybindConfig

--- VanillaPlus/Utilities/Drawing.cs
+++ VanillaPlus/Utilities/Drawing.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Dalamud.Bindings.ImGui;
-using Dalamud.Game.ClientState.Keys;
+﻿using Dalamud.Bindings.ImGui;
 using Dalamud.Interface;
 using Dalamud.Interface.Utility;
 using Dalamud.Interface.Utility.Raii;
@@ -14,39 +10,5 @@ public static class Drawing {
         using var pushedId = ImRaii.PushId(id);
         using var fixedFont = Services.PluginInterface.UiBuilder.IconFontFixedWidthHandle.Push();
         return ImGui.Button(icon.ToIconString(), ImGuiHelpers.ScaledVector2(25.0f, 25.0f));
-    }
-
-    public static bool KeybindConfig(HashSet<VirtualKey> keys, ref VirtualKey newKey) {
-        var result = false;
-        
-        foreach (var keybind in keys.ToList()) {
-            if (IconButton(FontAwesomeIcon.Trash, keybind.ToString())) {
-                keys.Remove(keybind);
-                result = true;
-            }
-            
-            ImGui.SameLine();
-            
-            ImGui.Text(keybind.ToString());
-        }
-
-        if (IconButton(FontAwesomeIcon.Plus, "newKey")) {
-            keys.Add(newKey);
-            result = true;
-        }
-        
-        ImGui.SameLine();
-
-        using var dropDown = ImRaii.Combo("##newKeyCombo", newKey.ToString(), ImGuiComboFlags.HeightLarge | ImGuiComboFlags.PopupAlignLeft);
-        if (dropDown) {
-            foreach (var key in Enum.GetValues<VirtualKey>()) {
-                if (ImGui.Selectable(key.ToString(), key == newKey)) {
-                    newKey = key;
-                    result = true;
-                }
-            }
-        }
-        
-        return result;
     }
 }
