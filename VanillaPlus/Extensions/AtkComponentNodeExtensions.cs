df9e5472ce6352dc5ed93463e0ec74fc5ca06918
MidoriKami <mi_kami@outlook.com>
2025-08-28 21:18:39 -0700

    Add AtkComponentNode Extensions

---
+++ VanillaPlus/Extensions/AtkComponentNodeExtensions.cs
@@ -0,0 +1,8 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace VanillaPlus.Extensions;
+
+public static unsafe class AtkComponentNodeExtensions {
+    public static T* SearchNodeById<T>(ref this AtkComponentNode node, uint id) where T : unmanaged
+        => node.Component->UldManager.SearchNodeById<T>(id);
+}
