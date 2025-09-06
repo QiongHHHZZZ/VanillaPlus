8b5fd459cd41405c68c8b25701ba41e20e5484a5
MidoriKami <mi_kami@outlook.com>
2025-09-06 13:32:08 -0700

    Add Component FadeNode extension

--- VanillaPlus/Extensions/AtkComponentNodeExtensions.cs
+++ VanillaPlus/Extensions/AtkComponentNodeExtensions.cs
@@ -5,4 +5,10 @@ namespace VanillaPlus.Extensions;
 public static unsafe class AtkComponentNodeExtensions {
     public static T* SearchNodeById<T>(ref this AtkComponentNode node, uint id) where T : unmanaged
         => node.Component->UldManager.SearchNodeById<T>(id);
+
+    public static void FadeNode(ref this AtkComponentNode node, float fadePercentage) {
+        node.MultiplyRed = (byte) ((1 - fadePercentage) * 100);
+        node.MultiplyGreen = (byte) ((1 - fadePercentage) * 100);
+        node.MultiplyBlue = (byte) ((1 - fadePercentage) * 100);
+    }
 }
