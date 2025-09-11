68759be56d6efa66bbf316b752d0111aed2dfea9
MidoriKami <mi_kami@outlook.com>
2025-09-10 19:51:41 -0700

    Add GetCurrentRetainerSorter

---
+++ VanillaPlus/Extensions/ItemOrderModuleExtensions.cs
@@ -0,0 +1,8 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+
+namespace VanillaPlus.Extensions;
+
+public static unsafe class ItemOrderModuleExtensions {
+    public static ItemOrderModuleSorter* GetCurrentRetainerSorter(ref this ItemOrderModule instance)
+        => instance.RetainerSorter[instance.ActiveRetainerId];
+}
