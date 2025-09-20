8ebe0cec810632c00120252751beddad6553990e
MidoriKami <mi_kami@outlook.com>
2025-09-20 13:42:21 -0700

    Add AtkValue Printer

---
+++ VanillaPlus/Extensions/AtkValueSpanExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace VanillaPlus.Extensions;
+
+public static class AtkValueSpanExtensions {
+    public static void PrintValues(this Span<AtkValue> values, int indentSpaces = 0) {
+        foreach (var index in Enumerable.Range(0, values.Length)) {
+            ref var value = ref values[index];
+
+            Services.PluginLog.Debug($"{new string(' ', indentSpaces)}[{index}] [{value.Type}] {value.GetValueAsString()}");
+        }
+    }
+}
