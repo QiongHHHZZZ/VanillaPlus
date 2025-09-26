1bbc3ea9a58c356f828e5bedb5dd5009ec3598e3
MidoriKami <mi_kami@outlook.com>
2025-09-26 08:25:24 -0700

    Add nullable helper to AddonEventManager

---
+++ VanillaPlus/Extensions/AddonEventManagerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Dalamud.Game.Addon.Events;
+using Dalamud.Plugin.Services;
+
+namespace VanillaPlus.Extensions;
+
+public static class AddonEventManagerExtensions {
+    public static void RemoveEventNullable(this IAddonEventManager manager, IAddonEventHandle? handle) {
+        if (handle is not null) {
+            manager.RemoveEvent(handle);
+        }
+    }
+}
