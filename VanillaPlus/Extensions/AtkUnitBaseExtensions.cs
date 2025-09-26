5e75eb5be2695f4b6952d8a4a420c43ef342f03d
MidoriKami <mi_kami@outlook.com>
2025-09-26 08:25:14 -0700

    Add atkunitbase shortcut for SearchNodeById<T>

---
+++ VanillaPlus/Extensions/AtkUnitBaseExtensions.cs
@@ -0,0 +1,8 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace VanillaPlus.Extensions;
+
+public static unsafe class AtkUnitBaseExtensions {
+    public static T* GetNodeById<T>(this ref AtkUnitBase addon, uint nodeId) where T : unmanaged
+        => addon.UldManager.SearchNodeById<T>(nodeId);
+}
