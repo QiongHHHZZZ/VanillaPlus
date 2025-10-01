c26450f5e23d51da59e4ae6abdd6b93c0dfac2b8
MidoriKami <mi_kami@outlook.com>
2025-09-30 20:28:18 -0700

    Add IsPetOrOwner to GameObjectExtensions

--- VanillaPlus/Extensions/GameObjectExtensions.cs
+++ VanillaPlus/Extensions/GameObjectExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Dalamud.Game.ClientState.Objects.Enums;
+﻿using System.Linq;
+using Dalamud.Game.ClientState.Objects.Enums;
 using Dalamud.Game.ClientState.Objects.Types;
 
 namespace VanillaPlus.Extensions;
@@ -6,4 +7,12 @@ namespace VanillaPlus.Extensions;
 public static class GameObjectExtensions {
     public static bool IsPet(this IGameObject gameObject)
         => gameObject is { ObjectKind: ObjectKind.BattleNpc, SubKind: (byte)BattleNpcSubKind.Pet };
+    
+    public static bool IsPetOrOwner(this IGameObject? gameObject) {
+        if (gameObject is null) return false;
+        if (gameObject.IsPet()) return true;
+        if (Services.ObjectTable.ClientObjects.Any(obj => obj.IsPet() && obj.OwnerId == gameObject.EntityId)) return true;
+
+        return false;
+    }
 }
