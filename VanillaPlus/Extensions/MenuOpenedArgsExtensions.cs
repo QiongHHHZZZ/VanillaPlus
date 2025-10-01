06aef6a48c343a6e8821aeb2b4f3e010ca80f2dd
MidoriKami <mi_kami@outlook.com>
2025-09-30 20:28:42 -0700

    Add unused helper

---
+++ VanillaPlus/Extensions/MenuOpenedArgsExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Dalamud.Game.Gui.ContextMenu;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+namespace VanillaPlus.Extensions;
+
+public static unsafe class MenuOpenedArgsExtensions {
+    public static AgentContext* GetContext(this IMenuOpenedArgs args)
+        => (AgentContext*)args.AgentPtr;
+}
