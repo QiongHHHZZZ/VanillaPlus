bccb9131288985bea1b360f6cadc34b68c199b28
MidoriKami <mi_kami@outlook.com>
2025-10-05 15:43:03 -0700

    Add title case extensions

---
+++ VanillaPlus/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+namespace VanillaPlus.Extensions;
+
+public static class StringExtensions {
+    public static string ToTitleCase(this string str)
+        => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str);
+}
