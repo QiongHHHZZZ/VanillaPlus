2f8712e63c85b3b956b11b5c9a10de678d0e22f3
MidoriKami <mi_kami@outlook.com>
2025-10-11 19:22:21 -0700

    Add AtkResNode Position extension

--- VanillaPlus/Extensions/AtkResNodeExtensions.cs
+++ VanillaPlus/Extensions/AtkResNodeExtensions.cs
@@ -6,4 +6,7 @@ namespace VanillaPlus.Extensions;
 public static class AtkResNodeExtensions {
     public static Vector2 Size(this ref AtkResNode node)
         => new(node.Width, node.Height);
+    
+    public static Vector2 Position(this ref AtkResNode node)
+        => new(node.X, node.Y);
 }
