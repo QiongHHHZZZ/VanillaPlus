000ea7446d3d4f1b134f4f2ba401eca07f223663
MidoriKami <mi_kami@outlook.com>
2025-10-17 12:03:07 -0700

    add debug addon

---
+++ VanillaPlus/Features/DebugCustomAddon/DebugAddon.cs
@@ -0,0 +1,16 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Addon;
+
+namespace VanillaPlus.Features.DebugCustomAddon;
+
+#if DEBUG
+/// <summary>
+/// Debug Addon Window for use with playing around with ideas, DO NOT commit changes to this file
+/// </summary>
+public class DebugAddon : NativeAddon {
+    
+    protected override unsafe void OnSetup(AtkUnitBase* addon) {
+        
+    }
+}
+#endif
---
+++ VanillaPlus/Features/DebugCustomAddon/DebugCustomAddon.cs
@@ -0,0 +1,41 @@
+﻿using System.Numerics;
+using VanillaPlus.Classes;
+
+namespace VanillaPlus.Features.DebugCustomAddon;
+
+#if DEBUG
+/// <summary>
+/// Debug Game Modification with a Custom Addon for use with playing around with ideas, DO NOT commit changes to this file
+/// </summary>
+public class DebugCustomAddon : GameModification {
+    public override ModificationInfo ModificationInfo => new() {
+        DisplayName = "Debug CustomAddon",
+        Description = "A module for playing around and testing VanillaPlus features",
+        Type = ModificationType.Debug,
+        Authors = [ "YourNameHere" ],
+        ChangeLog = [
+            new ChangeLogInfo(1, "Initial Implementation"),
+        ],
+    };
+
+    private DebugAddon? debugAddon;
+
+    public override void OnEnable() {
+        debugAddon = new DebugAddon {
+            NativeController = System.NativeController,
+            InternalName = "DebugAddon",
+            Title = "Debug Addon Window",
+            Size = new Vector2(500.0f, 500.0f),
+        };
+
+        debugAddon.Open();
+
+        OpenConfigAction = debugAddon.Toggle;
+    }
+
+    public override void OnDisable() {
+        debugAddon?.Dispose();
+        debugAddon = null;
+    }
+}
+#endif
